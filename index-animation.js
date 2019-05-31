15885fab315a4ca4f6f45679764ac9e8bdce36ba
Janie Delhi <jandel36@gmail.com>
2019-05-30 20:53:45 -0700

    from scratch

---
+++ index-animation.js
@@ -0,0 +1,9 @@
+anime({
+  targets: '.text-1',
+  direction: 'alternate',
+  loop: true,
+  duration: 2000,
+  easing: 'linear',
+  opacity: 0
+});
+
--- index.md
+++
@@ -1,2 +0,0 @@
-## Janie Delhi
-Software Developer | Seattle, WA
